0de4f8fd1c884d757ee0257d41246d3ac6e270be
Cortlyn Stafford <cortlynstafford@icloud.com>
2023-03-31 14:35:41 -0400

    completed array filter lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+const drivers = [
+    { name: 'Pat', hometown: 'Atlanta' },
+    { name: 'CJ', hometown: 'Los Angeles' },
+    { name: 'Tammy', hometown: 'Houston' },
+    { name: 'Ray', hometown: 'New York' }
+  ];
+  
+  function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
